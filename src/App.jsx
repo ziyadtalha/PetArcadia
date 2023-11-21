b4c2aaee2b9a20d6105224d1f6a747446ef5ef7a
ziyadtalha <ziyadtalha24@gmail.com>
2023-11-21 21:18:42 +0500

    worked on new responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,17 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import SignIn from './pages/SignIn';
-import SignUp from "./pages/SignUp";
-import Home from "./pages/Home";
-import ViewPets from "./pages/ViewPets"
+//import SignIn from './pages/SignIn';
+// import SignUp from "./pages/SignUp";
+// import Home from "./pages/Home";
+// import ViewPets from "./pages/ViewPets"
 
-import ResponsiveAppBar from './components/ResponsiveAppBar';
+import ResponsiveNavBar from './components/ResponsiveNavbar';
 
 function App() {
   return (
     <>
+      <ResponsiveNavBar />
 
-    <Router>
-
-      <ResponsiveAppBar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<SignUp />} />
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/viewpets" element={<ViewPets />} />
-      </Routes>
-
-    </Router>
     </>
   );
 }
